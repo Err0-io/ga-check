8e467fcb2174d78ee9ca3a11a59ab2f7811ebc0c
Rafael Madolell <rafael.madolell@cartier.com>
2024-02-01 18:24:56 +0100

    Reff defined

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash 
-
-# Configura el token de acceso personal
-# GIT_TOKEN=$YOUR_GIT_TOKEN  # Reemplaza con tu propio token
-
+# turn the detached message off
+git config --global advice.detachedHead false
 # Clona el repositorio dentro del contenedor
 git clone https://doe:$GH_TOKEN@$REPO /repo
 
@@ -11,8 +9,12 @@ git clone https://doe:$GH_TOKEN@$REPO /repo
 cd /repo
 
 # Realiza el checkout del código deseado
+
 git checkout $GITHUB_REF
 
 # Tu código de análisis a continuación
 echo $ERR0_JSON | base64 -d >> /tmp/err0agent.json
 err0.sh --token-file /tmp/err0agent.json --check --git-dir /repo
+
+
+
